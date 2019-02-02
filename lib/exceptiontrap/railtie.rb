95e269e9236f588d83fa709c3d759d45043b5670
tbuehl <tbuehl@itm-labs.de>
2019-02-02 14:08:57 +0100

    Fix for Rails 5
    Don’t use strings to specify the middleware for insert_after

--- lib/exceptiontrap/railtie.rb
+++ lib/exceptiontrap/railtie.rb
@@ -7,7 +7,7 @@ module Exceptiontrap
     initializer 'exceptiontrap.middleware' do |app|
       Exceptiontrap::Config.load(File.join(Rails.root, '/config/exceptiontrap.yml'))
       if Exceptiontrap::enabled?
-        app.config.middleware.insert_after 'ActionDispatch::ShowExceptions', 'Exceptiontrap::Rack'
+        app.config.middleware.insert_after ActionDispatch::ShowExceptions, Exceptiontrap::Rack
       end
     end
   end
